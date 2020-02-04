8548ae81897e84435d98621845ddc87a395735f7
Liren Yeo <liren90@gmail.com>
2020-02-04 14:40:59 +0800

    Apple feature complete

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def main():
+    print('This is initial code')
+
     print('Feature from banana branch')
     print('This is banana branch code')
 
+    print('This is apple..')
+    x = 'Keeps the doctor away'
+    print(x + '!!')
